10e0bf016e9182cf989e7fbe0ed952c1ae16af39
DrVipinKumar <geniusvipin@gmail.com>
2026-02-04 14:11:14 +0530

    Updated on 4th Feb, 2026

---
+++ AJPSecC/calc/Calculator.java
@@ -0,0 +1,17 @@
+package calc;
+
+class Sum {
+	int num1, num2;
+	void getSum() {
+		System.out.println("Sum="+(num1+num2));
+	}
+}
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              Sum s1=new Sum();
+              s1.getSum();
+	}
+
+}
